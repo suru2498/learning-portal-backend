eb4b621f71bfc1755b299089a822b62864ac90bd
Suraj Singh Kanyal <surajsinghkanyal@Rayes-MacBook-Air-M4.local>
2026-03-08 12:56:48 +0530

    added build scripts for render

--- src/index.ts
+++ src/index.ts
@@ -26,7 +26,7 @@ app.get("/", (req, res) => {
   res.send("Server Running");
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 7777;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
